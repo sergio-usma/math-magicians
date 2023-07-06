54b8b56990938bb2e6541bd6612fed708b47533f
sergio-usma <sergioandresusma@hotmail.com>
2023-07-06 12:04:34 -0500

    Add new component to the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import FetchApi from './components/FetchApi';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <FetchApi />
     </div>
   );
 }
